7b7dd9ee9c6cdc8671768e0e04b2e453204c310d
Mahm0udAbdelrahman <mahmudabdelrahman0@gmail.com>
2024-10-02 12:36:01 +0300

    update

--- routes/api.php
+++ routes/api.php
@@ -38,7 +38,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+Route::group(['prefix' => "auth"], function () {
+    Route::post('register',[AuthController::class,'register']);
+});
 
 
 
